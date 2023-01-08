3b436ee7248e79c5346711a72e8b862c85d02e2f
Wataru Hori <bireleysaddiction@gmail.com>
2023-01-08 18:35:51 +0900

    Initial commit

---
+++ public/main.js
@@ -0,0 +1,3 @@
+const update = document.getElementById('submit-button');
+
+update.addEventListener('click', console.log('Submit Button Clicked'));
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+MongoClient.connect(process.env.MONGO_STRING, 
+  { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database');
+    
+    const db = client.db('crud-task-tracker');
+    const tasksCollection = db.collection('tasks');
+    
+    app.get('/', (req, res) => {
+      db.collection('tasks').find().toArray()
+      .then(result => {
+        res.render('index.ejs', { tasks: result });
+      })
+      .catch(error => console.error(error))
+    });
+  
+    app.post('/tasks', (req, res) => {
+      tasksCollection.insertOne(req.body)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(error => console.error(error));
+    });
+  
+    app.listen(PORT, () => {
+      console.log('server running on ', PORT);
+    });
+  })
+  .catch(error => console.error(error));
+
+
+
+
